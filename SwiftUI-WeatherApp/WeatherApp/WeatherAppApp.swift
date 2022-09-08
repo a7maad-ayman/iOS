0db89934b97455b51a0bb9fbf6475f16213c884c
Ahmad Ayman Mansour <87352168+a7maad-ayman@users.noreply.github.com>
2022-09-08 21:33:59 +0200

    Simple weatherApp using SwiftUI
    Sean Allen SwiftUI tutorial

---
+++ SwiftUI-WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ahmad Ayman on 2021-11-30.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
